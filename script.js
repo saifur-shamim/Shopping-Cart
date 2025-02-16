5ac5495657bde83e9807cbf2fa650ea8bc1bb521
“saifur-shamim” <saifurshamim150@gmail.com>
2025-02-16 23:20:05 +0600

    shopping cart project

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const cartIcon = document.querySelector("#cart-icon");
 const cart = document.querySelector(".cart");
 const cartClose = document.querySelector("#cart-close");
 
+
 cartIcon.addEventListener("click", () => cart.classList.add("active"));
 cartClose.addEventListener("click", () => cart.classList.remove("active"));
 
